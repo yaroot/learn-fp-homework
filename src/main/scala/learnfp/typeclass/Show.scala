51c799e95491e7d6f0e7538c02778ae765a3566f
tsu <tsu@yaroot.net>
2018-01-05 22:12:09 +0800

    ShowTest

--- src/main/scala/learnfp/typeclass/Show.scala
+++ src/main/scala/learnfp/typeclass/Show.scala
@@ -12,14 +12,14 @@ object Printer {
 
 object ShowInstances {
   implicit val intInstance:Show[Int] = new Show[Int] {
-    override def show(x: Int): String = ???
+    override def show(x: Int): String = x.toString
   }
 
   implicit val doubleInstance:Show[Double] = new Show[Double] {
-    override def show(x: Double): String = ???
+    override def show(x: Double): String = x.toString
   }
 
   implicit def listInstance[T](implicit xShow:Show[T]):Show[List[T]] = new Show[List[T]] {
-    override def show(xs:List[T]): String = ???
+    override def show(xs:List[T]): String = xs.map(xShow.show).mkString("[", ", ", "]")
   }
 }
