c441c5f55588fabe6a0b036d452bd8422066ed72
tsu <tsu@yaroot.net>
2018-01-05 22:20:13 +0800

    Monoid

--- src/main/scala/learnfp/monoid/PairAdditiveMonoid.scala
+++ src/main/scala/learnfp/monoid/PairAdditiveMonoid.scala
@@ -6,8 +6,8 @@ object PairAdditiveMonoid {
 
   implicit def nestedMonoidInstance[A, B](implicit aMonoid:Monoid[A], bMonoid:Monoid[B]):Monoid[Pair[A, B]] =
     new Monoid[Pair[A, B]] {
-      override def mzero: Pair[A, B] = ???
+      override def mzero: Pair[A, B] = Pair(aMonoid.mzero, bMonoid.mzero)
       override def mappend(lhs: Pair[A, B], rhs: Pair[A, B]): Pair[A, B] =
-        ???
+        Pair(aMonoid.mappend(lhs.a, rhs.a), bMonoid.mappend(lhs.b, rhs.b))
   }
 }
