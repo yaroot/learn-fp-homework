53916d06a0cbf8f75e1374300fc1b92175eb95c2
tsu <tsu@yaroot.net>
2018-01-13 19:04:41 +0800

    transformers

--- src/main/scala/learnfp/transformer/IdT.scala
+++ src/main/scala/learnfp/transformer/IdT.scala
@@ -2,24 +2,36 @@ package learnfp.transformer
 
 import learnfp.functor.{Functor, FunctorOps, Id}
 import learnfp.monad.{Monad, MonadOps, MonadOpsPure}
+import learnfp.functor.IdInstance._
+import learnfp.functor.FunctorOps._
+import learnfp.monad.MonadOps._
 
 case class IdT[A, F[_]](runIdT:F[Id[A]])
 
 object IdT {
   implicit def idtFunctorInstance[F[_]](implicit outerFunctor:Functor[F]) = new Functor[({type E[X] = IdT[X, F]})#E] {
-    override def fmap[A, B](a: IdT[A, F])(fx: A => B): IdT[B, F] = ???
+    override def fmap[A, B](a: IdT[A, F])(fx: A => B): IdT[B, F] = {
+      IdT(a.runIdT.fmap(_.fmap(fx)))
+    }
   }
 
   implicit def idtMonadInstance[M[_]](implicit outerMonad:Monad[M], outerFunctor:Functor[M]) = new Monad[({type E[X] = IdT[X, M]})#E] {
-    override def pure[A](a: A): IdT[A, M] = ???
-    override def flatMap[A, B](a: IdT[A, M])(fx: A => IdT[B, M]): IdT[B, M] = ???
+    override def pure[A](a: A): IdT[A, M] = IdT(Id(a).pure[M])
+    override def flatMap[A, B](a: IdT[A, M])(fx: A => IdT[B, M]): IdT[B, M] = {
+      val bm = a.runIdT.flatMap { fa =>
+        fa.map(fx).value.runIdT
+      }
+      IdT(bm)
+    }
   }
 
   implicit def idtToMonadOps[A, M[_]](a:IdT[A, M])(implicit m:Monad[M], f:Functor[M]) =
     new MonadOps[A, ({type E[X] = IdT[X, M]})#E](a)
 
   implicit def idtMonadTransInstance[M[_]](implicit m:Monad[M], f:Functor[M]) = new MonadTransformer[M, IdT] {
-    override def lift[A](a: M[A]): IdT[A, M] = ???
+    override def lift[A](a: M[A]): IdT[A, M] = {
+      IdT(a.map(Id(_)))
+    }
   }
 
   def lift[A, M[_]](a:M[A])(implicit f:Functor[M], m:Monad[M]):IdT[A, M] = idtMonadTransInstance[M].lift(a)
--- src/main/scala/learnfp/transformer/MaybeT.scala
+++ src/main/scala/learnfp/transformer/MaybeT.scala
@@ -1,31 +1,47 @@
 package learnfp.transformer
 
 import learnfp.functor.Functor
-import learnfp.functor.Maybe.{Just, Maybe, Nothing}
+import learnfp.functor.Maybe.{Just, Maybe, Nothing, nothing, just}
 import learnfp.monad.{Monad, MonadOps}
 import learnfp.functor.FunctorOps._
 import learnfp.monad.MonadOps._
+import learnfp.functor.MaybeInstance._
+import learnfp.monad.MaybeInstance._
 
 case class MaybeT[A, M[_]](runMaybeT:M[Maybe[A]])(implicit f:Functor[M], m:Monad[M])
 
 object MaybeT {
   implicit def maybeTFunctorInstance[M[_]](implicit f:Functor[M], m:Monad[M]) = new Functor[({type E[X] = MaybeT[X, M]})#E] {
-    override def fmap[A, B](a: MaybeT[A, M])(fx: A => B): MaybeT[B, M] = ???
+    override def fmap[A, B](a: MaybeT[A, M])(fx: A => B): MaybeT[B, M] = {
+      val run = a.runMaybeT
+      val mm = run.map(_.fmap(fx))
+      MaybeT(mm)
+    }
   }
 
   implicit def maybeTMonadInstance[M[_]](implicit f:Functor[M], m:Monad[M]) = new Monad[({type E[X] = MaybeT[X, M]})#E]() {
-    override def pure[A](a: A): MaybeT[A, M] = ???
-    override def flatMap[A, B](a: MaybeT[A, M])(fx: A => MaybeT[B, M]): MaybeT[B, M] = ???
+    override def pure[A](a: A): MaybeT[A, M] = {
+      MaybeT(a.pure[Maybe].pure[M])
+    }
+    override def flatMap[A, B](a: MaybeT[A, M])(fx: A => MaybeT[B, M]): MaybeT[B, M] = {
+      val run = a.runMaybeT.flatMap {
+        case Just(x) => fx(x).runMaybeT
+        case Nothing() => m.pure(nothing[B]())
+      }
+      MaybeT(run)
+    }
   }
 
   implicit def maybeTToMonadOps[A, M[_]](a:MaybeT[A, M])(implicit m:Monad[M], f:Functor[M]) =
     new MonadOps[A, ({type E[X] = MaybeT[X, M]})#E](a)
 
   implicit def maybeTMonadTransInstance[M[_]](implicit f:Functor[M], m:Monad[M]) = new MonadTransformer[M, MaybeT] {
-    override def lift[A](a: M[A]): MaybeT[A, M] = ???
+    override def lift[A](a: M[A]): MaybeT[A, M] = {
+      MaybeT(a.fmap(just))
+    }
   }
 
-  def nothingT[A, M[_]](implicit f:Functor[M], m:Monad[M]):MaybeT[A, M] = ???
+  def nothingT[A, M[_]](implicit f:Functor[M], m:Monad[M]):MaybeT[A, M] = MaybeT(m.pure(Nothing()))
 
-  def lift[A, M[_]](a:M[A])(implicit f:Functor[M], m:Monad[M]):MaybeT[A, M] = maybeTMonadTransInstance.lift(a)
+  def lift[A, M[_]](a:M[A])(implicit f:Functor[M], m:Monad[M]):MaybeT[A, M] = maybeTMonadTransInstance(f, m).lift(a)
 }
