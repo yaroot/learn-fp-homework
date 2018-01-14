748778035f631b93d5075a4e041b204a9c8b8455
tsu <tsu@yaroot.net>
2018-01-14 14:53:48 +0800

    comonad

--- src/main/scala/learnfp/comonad/Id.scala
+++ src/main/scala/learnfp/comonad/Id.scala
@@ -5,8 +5,8 @@ import learnfp.functor.IdInstance._
 
 object IdInstance {
   implicit val idCoMonadInstance = new CoMonad[Id] {
-    override def counit[A](a: Id[A]): A = ???
-    override def coFlatMap[A, B](a: Id[A])(fx: Id[A] => B): Id[B] = ???
+    override def counit[A](a: Id[A]): A = a.value
+    override def coFlatMap[A, B](a: Id[A])(fx: Id[A] => B): Id[B] = Id(fx(a))
   }
 }
 
